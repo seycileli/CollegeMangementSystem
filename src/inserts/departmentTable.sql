3fa896f75af191cd168704f1a89b7206c0db55c4
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-02-04 23:16:50 -0500

    Create departmentTable.sql

---
+++ src/inserts/departmentTable.sql
@@ -0,0 +1,12 @@
+
+/* Insertions for table department */
+
+insert into department (id,name) values (1,'Arts & Design');
+insert into department (id,name) values (2,'Business');
+insert into department (id,name) values (3,'Culture & History');
+insert into department (id,name) values (4,'Engineering');
+insert into department (id,name) values (5,'Information Technology');
+insert into department (id,name) values (6,'Medicine');
+insert into department (id,name) values (7,'Sciences');
+insert into department (id,name) values (8,'Social Sciences');
+
