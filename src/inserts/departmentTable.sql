a1233a1bb8b0f11b95e4da83347a6dbbe6d4604c
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-02-04 23:19:06 -0500

    Update departmentTable.sql

--- src/inserts/departmentTable.sql
+++ src/inserts/departmentTable.sql
@@ -1,12 +1,12 @@
 
 /* Insertions for table department */
 
-insert into department (id,name) values (1,'Arts & Design');
-insert into department (id,name) values (2,'Business');
-insert into department (id,name) values (3,'Culture & History');
-insert into department (id,name) values (4,'Engineering');
-insert into department (id,name) values (5,'Information Technology');
-insert into department (id,name) values (6,'Medicine');
-insert into department (id,name) values (7,'Sciences');
-insert into department (id,name) values (8,'Social Sciences');
+insert into department (id,departmentName) values (1,'Arts & Design');
+insert into department (id,departmentName) values (2,'Business');
+insert into department (id,departmentName) values (3,'Culture & History');
+insert into department (id,departmentName) values (4,'Engineering');
+insert into department (id,departmentName) values (5,'Information Technology');
+insert into department (id,departmentName) values (6,'Medicine');
+insert into department (id,departmentName) values (7,'Sciences');
+insert into department (id,departmentName) values (8,'Social Sciences');
 
